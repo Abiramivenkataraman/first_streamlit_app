d5594e55f4231079263e618691654874157e7cfc
Abiramivenkataraman <141864845+Abiramivenkataraman@users.noreply.github.com>
2023-08-10 10:01:15 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,12 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, Spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free_range egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free_range egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
