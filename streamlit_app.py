55494e45c1eebbdba16152dad8943e5d3fdb1401
Abiramivenkataraman <141864845+Abiramivenkataraman@users.noreply.github.com>
2023-08-11 14:16:29 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,7 @@ import pandas
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('Thanks for adding', add_my_fruit)
 
-import snowflake.connector
-my_cur.execute("insert into fruit_load_list values(from streamlit)")
+
 
 
 
