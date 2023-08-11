944550a6e8f74cb86e6d5c36e1b17f88cd947e70
Abiramivenkataraman <141864845+Abiramivenkataraman@users.noreply.github.com>
2023-08-11 15:32:06 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','kiwi')
 streamlit.write('The user entered', fruit_choice)
 
+print("I am in 29")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+print("I am in 31")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe('fruityvice_normalized')
 
